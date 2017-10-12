9ea56a37d468607a3422b40d77cb689f071b1b5d
vladmal <vladimir.malacko@gmail.com>
2017-10-12 13:44:26 +0200

    Initializing Google Map API : Direction Service : static data

--- webapp/routes/routeDetails/routeDetailsController.js
+++ webapp/routes/routeDetails/routeDetailsController.js
@@ -18,12 +18,42 @@
 
         var map;
         GoogleMapInitializer.mapsInitialized
-            .then(function(res){
-                map = new google.maps.Map(document.getElementById('map'), {
-                    center: {lat: 45.253980, lng: 19.840815},
-                    zoom: 8
+            .then(function(){
+
+                var directionsService = new google.maps.DirectionsService();
+                var directionsDisplay = new google.maps.DirectionsRenderer();
+
+                var myOptions = {
+                    zoom:7,
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                };
+
+                map = new google.maps.Map(document.getElementById("map"), myOptions);
+                directionsDisplay.setMap(map);
+
+                var request = {
+                    origin: 'novi sad',
+                    destination: 'london',
+                    travelMode: google.maps.DirectionsTravelMode.DRIVING
+                };
+
+                directionsService.route(request, function(response, status) {
+                    if (status == google.maps.DirectionsStatus.OK) {
+
+                        var distanceInMeters = response.routes[0].legs[0].distance.value;
+                        var distance = distanceInMeters/1000;
+                        console.log(distance + " km.");
+
+                        var timeInSeconds = response.routes[0].legs[0].duration.value;
+                        var hours = Math.floor(timeInSeconds/3600);
+                        var minutes = timeInSeconds - hours*60;
+                        console.log("About " + hours + " hours and " + minutes + " minutes.");
+
+                        directionsDisplay.setDirections(response);
+                    }
                 });
-        })
+
+        });
 
     }
 
